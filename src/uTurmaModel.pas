6ca6a5672a4a2b4e18d2feb6af041d6d31c85bfb
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-08-12 14:02:22 -0300

    :sparkles: feat (TurmaModel): Construtor implementado

--- src/uTurmaModel.pas
+++ src/uTurmaModel.pas
@@ -23,6 +23,11 @@ implementation
 
 constructor TTurmaModel.Create(aDatabase: TDatabase;
   aProfessorModel: TProfessorModel; aDisciplinaModel: TDisciplinaModel);
+var
+  idTurma, idProfessor, idDisciplina: Integer;
+  turma: TTurma;
+  professor: TProfessor;
+  disciplina: TDisciplina;
 begin
   if aDatabase = nil then raise Exception.Create('TTurmaModel.Create: aDatabase não pode ser nulo');
   if aProfessorModel = nil then raise Exception.Create('TTurmaModel.Create: aProfessorModel não pode ser nulo');
@@ -35,6 +40,28 @@ begin
   Self.Turmas := TObjectList<TTurma>.Create;
 
   Self.Database.FDQuery.SQL.Text := 'SELECT * FROM turmas WHERE ativo=true';
+  Self.Database.FDQuery.Open;
+
+  while not Self.Database.FDQuery.Eof do begin
+    idProfessor := Self.Database.FDQuery.FieldByName('id_professor').AsInteger;
+    professor := ModelProfessor.GetProfessorById(idProfessor);
+
+    idDisciplina := Self.Database.FDQuery.FieldByName('id_disciplina').AsInteger;
+    disciplina := ModelDisciplina.GetDisciplinaById(idDisciplina);
+
+    idDisciplina := Self.Database.FDQuery.FieldByName('id').AsInteger;
+
+    turma := TTurma.Create;
+    turma.SetId(idDisciplina);
+    turma.SetProfessor(professor);
+    turma.SetDisciplina(disciplina);
+
+    Self.Turmas.Add(turma);
+
+    Self.Database.FDQuery.Next;
+  end;
+  Self.Database.FDQuery.Close;
+
 end;
 
 end.
